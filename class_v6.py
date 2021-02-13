42563cac05a4018b218a4c9a2544adeecf67009c
Kristaps Vilerts <45102551+vilerts@users.noreply.github.com>
2021-02-13 15:37:28 +0200

    Create class_v6.py

---
+++ class_v6.py
@@ -0,0 +1,38 @@
+import sys
+for path in sys.path:
+    print(path)
+
+#################################
+
+import sys
+print("\n".join(sys.builtin_module_names))
+
+#################################
+
+import fold_class_v6.func 
+radius = int(input("Enter radius: "))
+print(f"Area: {fold_class_v6.func.calculateSphereArea(radius)}")
+print(f"Volume: {fold_class_v6.func.calculateSphereVolume(radius)}")
+
+#from fold_class_v6.func import calculateSphereArea, calculateSphereVolume
+#radius = int(input("Enter radius: "))
+#print(f"Area: {calculateSphereArea(radius)}")
+#print(f"Volume: {calculateSphereVolume(radius)}")
+
+#from fold_class_v6.func import *
+#radius = int(input("Enter radius: "))
+#print(f"Area: {calculateSphereArea(radius)}")
+#print(f"Volume: {calculateSphereVolume(radius)}")
+
+
+#################################
+
+from fold_class_v6.parent_package.package2 import calculator,format_expression
+
+var1 = int(input("Enter first number: "))
+var2 = int(input("Enter second number: "))
+operator = str(input("Enter operator: "))
+result = calculator.runCalculator(var1, var2, operator)
+print(format_expression.formatResult(var1,var2,operator,result))
+
+#################################
