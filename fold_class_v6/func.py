4568685cd15b47baedcc278f39e86f53351624b1
Kristaps Vilerts <45102551+vilerts@users.noreply.github.com>
2021-02-13 15:42:14 +0200

    Create func.py

---
+++ fold_class_v6/func.py
@@ -0,0 +1,10 @@
+import math
+
+def calculateSphereArea(radius):
+  area = 4 * math.pi * (radius**2)  
+  return area
+
+def calculateSphereVolume(radius):
+  volume = 4 / 3 * math.pi * (radius**3)
+  return volume
+  
